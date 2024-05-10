881c586d86d7e0cac62501ced34e7a9918385230
hamzaejaz787 <hejaz389@gmail.com>
2024-05-10 16:45:37 +0500

    fixed header positioning

--- app/dell_e5440/page.tsx
+++ app/dell_e5440/page.tsx
@@ -1,27 +1,25 @@
-'use client'
-import Image from 'next/image';
-import React, {useState} from 'react';
-import { MdNavigateNext, MdOutlineScreenshot, MdOutlineStorage } from "react-icons/md";
+"use client";
+import Image from "next/image";
+import React, { useState } from "react";
+import {
+  MdNavigateNext,
+  MdOutlineScreenshot,
+  MdOutlineStorage,
+} from "react-icons/md";
 import { SiTicktick } from "react-icons/si";
 import { IoHardwareChipOutline } from "react-icons/io5";
-import { Separator } from '@/components/ui/separator';
+import { Separator } from "@/components/ui/separator";
 import { FaMinus, FaPlus } from "react-icons/fa6";
-import { Button } from '@/components/ui/button';
-import Laptop_card from '@/components/Laptop_card';
-import { Input } from '@/components/ui/input';
+import { Button } from "@/components/ui/button";
+import Laptop_card from "@/components/Laptop_card";
+import { Input } from "@/components/ui/input";
 import { IoMdSearch } from "react-icons/io";
-import { AccordionFAQ } from '@/components/Accordion';
-
-
-
-
-  
+import { AccordionFAQ } from "@/components/Accordion";
 
 function Dell_E5440() {
   const [activeButton, setActiveButton] = useState(0);
 
-  const FAQ_buttoncontents = <AccordionFAQ/>;
-
+  const FAQ_buttoncontents = <AccordionFAQ />;
 
   const handleButtonClick = (index: any) => {
     setActiveButton(index);
@@ -29,18 +27,18 @@ function Dell_E5440() {
 
   const buttonContents = [
     `We at Laptop Rentals understand whether you're hosting a conference, trade show, or business event, having access to reliable and high-quality technology can make all.We at Laptop Rentals understand whether you'.reliable and high-quality technology can make all.We at Laptop Rentals understand whether you'.understand whether you'.reliable and high-quality technology can make all.We at Laptop Rentals understand whether you'.We at Laptop Rentals understand whether you're hosting a conference, trade show, or business event, having access to reliable and high-quality technology can make all.We at Laptop Rentals understand whether you'.reliable and high-quality technology can make all.We at Laptop Rentals understand whether you'.understand whether you'.reliable and high-quality technology can make all.We at Laptop Rentals understand whether you'.We at Laptop Rentals understand whether you're hosting a conference, trade show, or business event, having access to reliable and high-quality technology can make all.We at Laptop Rentals understand whether you'.reliable and high-quality technology can make all.We at Laptop Rentals understand whether you'.understand whether you'.reliable and high-quality technology can make all.We at Laptop Rentals understand whether you'.We at Laptop Rentals understand whether you're hosting a conference, trade show, or business event, having access to reliable and high-quality technology can make all.We at Laptop Rentals understand whether you'.reliable and high-quality technology can make all.We at Laptop Rentals understand whether you'.understand whether you'.reliable and high-quality technology can make all.We at Laptop Rentals understand whether you'.We at Laptop Rentals understand whether you're hosting a conference, trade show, or business event, having access to reliable and high-quality technology can make all.We at Laptop Rentals understand whether you'.reliable and high-quality technology can make all.We at Laptop Rentals understand whether you'.understand whether you'.reliable and high-quality technology can make all.We at Laptop Rentals understand whether you'.`,
-    'This is a guide on how to use the product.',
-    'These are reviews from customers who have used the product.',
+    "This is a guide on how to use the product.",
+    "These are reviews from customers who have used the product.",
   ];
   return (
-    <div className='flex flex-col'>
-      <div className='relative '>
-        <Image src='/dell_bg.png' alt='' width={3000} height={50} />
+    <>
+      <div className="relative">
+        <Image src="/dell_bg.png" alt="" width={3000} height={50} />
 
         {/* Content */}
-        <div className='absolute inset-0 flex flex-col items-center justify-center'>
-          <p className='font-bold text-white text-[40px]'>DELL E5440 LAPTOP</p>
-          <div className='flex text-white items-center font-medium text-[16px]'>
+        <div className="absolute inset-0 flex flex-col items-center justify-center">
+          <p className="font-bold text-white text-[40px]">DELL E5440 LAPTOP</p>
+          <div className="flex text-white items-center font-medium text-[16px]">
             <p>Home</p>
             <MdNavigateNext />
             <p>Laptop</p>
@@ -53,118 +51,137 @@ function Dell_E5440() {
       </div>
 
       {/* Photo Frame and Laptop */}
-      <div className='flex pt-20 pl-20'>
+      <div className="flex pt-20 pl-20">
         <div>
-          <Image src="/imageframe.png" alt="" width={1280} height={1280}/>
+          <Image src="/imageframe.png" alt="" width={1280} height={1280} />
         </div>
-        <div className='flex flex-col pl-10 pt-5 pr-20 gap-3'>
-          <p className='font-extrabold  text-[34px]'>DELL E5440 LATITUDE</p>
-          <p className='line-clamp-2 text-gray text-[15px]'>We at Laptop Rentals understand whether you are hosting a conference, trade show, or business event, having access to a conference, trade.</p>
-          <div className='flex flex-col gap-2'>
-          <div className='flex items-center gap-6 font-medium text-[14px]'>
-          <SiTicktick color='D61837'/>
-          <p>We at Laptop Rentals understand</p>
+        <div className="flex flex-col pl-10 pt-5 pr-20 gap-3">
+          <p className="font-extrabold  text-[34px]">DELL E5440 LATITUDE</p>
+          <p className="line-clamp-2 text-gray text-[15px]">
+            We at Laptop Rentals understand whether you are hosting a
+            conference, trade show, or business event, having access to a
+            conference, trade.
+          </p>
+          <div className="flex flex-col gap-2">
+            <div className="flex items-center gap-6 font-medium text-[14px]">
+              <SiTicktick color="D61837" />
+              <p>We at Laptop Rentals understand</p>
+            </div>
+            <div className="flex items-center gap-6 font-medium text-[14px]">
+              <SiTicktick color="D61837" />
+              <p>We at Laptop Rentals understand</p>
+            </div>
+            <div className="flex items-center gap-6 font-medium text-[14px]">
+              <SiTicktick color="D61837" />
+              <p>We at Laptop Rentals understand</p>
+            </div>
+            <div className="flex items-center gap-6 font-medium text-[14px]">
+              <SiTicktick color="D61837" />
+              <p>We at Laptop Rentals understand</p>
+            </div>
           </div>
-          <div className='flex items-center gap-6 font-medium text-[14px]'>
-          <SiTicktick color='D61837'/>
-          <p>We at Laptop Rentals understand</p>
+          <p className="font-extrabold text-[20px]">Luggages Include</p>
+          <div className="flex gap-8">
+            <div className="flex flex-col h-28 w-40 border-gray-300 rounded-md border-2 items-center justify-center">
+              <IoHardwareChipOutline color="D61837" size={60} />
+              <p className="text-[16px] font-light">Charger</p>
+            </div>
+            <div className="flex flex-col h-28 w-40 border-gray-300 rounded-md border-2 items-center justify-center">
+              <MdOutlineStorage color="D61837" size={60} />
+              <p className="text-[16px] font-light">Laptop</p>
+            </div>
+            <div className="flex flex-col h-28 w-40 border-gray-300 rounded-md border-2 items-center justify-center">
+              <MdOutlineScreenshot color="D61837" size={60} />
+              <p className="text-[16px] font-light">Power Cable</p>
+            </div>
           </div>
-          <div className='flex items-center gap-6 font-medium text-[14px]'>
-          <SiTicktick color='D61837'/>
-          <p>We at Laptop Rentals understand</p>
+          <div className="flex items-center">
+            <div className="flex border-gray-700 rounded-sm h-12 w-48 border-2 items-center justify-center gap-6">
+              <FaMinus />
+              <Separator orientation="vertical" color="gray" />
+              <p className="font-semibold text-[16px]">1</p>
+              <Separator orientation="vertical" />
+              <FaPlus />
+            </div>
+            <p className="font-semibold text-[20px] pl-5">Qty</p>
           </div>
-          <div className='flex items-center gap-6 font-medium text-[14px]'>
-          <SiTicktick color='D61837'/>
-          <p>We at Laptop Rentals understand</p>
+          <div className="flex h-10 ">
+            <div className="flex bg-[#D61837] rounded-l-sm  items-center pl-10 pr-10 justify-center text-white font-medium text-[16px] w-88">
+              Request Quote
+            </div>
+            <div className="flex bg-black w-20 items-center rounded-r-sm justify-center">
+              <FaPlus color="white" size={25} />
+            </div>
           </div>
-          </div>
-          <p className='font-extrabold text-[20px]'>Luggages Include</p>
-          <div className='flex gap-8'>
-          <div className='flex flex-col h-28 w-40 border-gray-300 rounded-md border-2 items-center justify-center'>
-          <IoHardwareChipOutline color='D61837' size={60}/>
-          <p className='text-[16px] font-light'>Charger</p>
-          </div>
-          <div className='flex flex-col h-28 w-40 border-gray-300 rounded-md border-2 items-center justify-center'>
-          <MdOutlineStorage color='D61837' size={60}/>
-          <p className='text-[16px] font-light'>Laptop</p>
-          </div>
-          <div className='flex flex-col h-28 w-40 border-gray-300 rounded-md border-2 items-center justify-center'>
-          <MdOutlineScreenshot  color='D61837' size={60}/>
-          <p className='text-[16px] font-light'>Power Cable</p>
-          </div>
-          </div>
-          <div className='flex items-center'>
-          <div className='flex border-gray-700 rounded-sm h-12 w-48 border-2 items-center justify-center gap-6'>
-          <FaMinus />
-          <Separator orientation='vertical' color='gray'/>
-          <p className='font-semibold text-[16px]'>1</p>
-          <Separator orientation='vertical'/>
-          <FaPlus />
-          </div>
-          <p className='font-semibold text-[20px] pl-5'>Qty</p>
-          </div>
-          <div className='flex h-10 '>
-            <div className='flex bg-[#D61837] rounded-l-sm  items-center pl-10 pr-10 justify-center text-white font-medium text-[16px] w-88'>Request Quote</div>
-            <div className='flex bg-black w-20 items-center rounded-r-sm justify-center'><FaPlus color='white' size={25}/></div>
-          </div>
-          
         </div>
       </div>
       <div className="flex flex-col gap-4 pt-10">
-      <div className="flex font-semibold text-[16px] justify-center">
-      {['Description', 'Product Guide', 'Reviews'].map((label, index) => (
-        <Button
-          key={index}
-          variant="link"
-          className={`${activeButton === index? 'text-red-600' : ''} flex`}
-          onClick={() => handleButtonClick(index)}
-        >
-          {label}
-        </Button>
-      ))}
-      </div>
-      <div className="mt-4 ml-20 mr-20 font-medium text-[14px]">
-        {buttonContents[activeButton]}
-      </div>
-    </div>
-    <p className='text-[40px] font-bold text-center'>Related Products</p>
-    <div className='flex gap-4 items-center justify-center'>
-    <Laptop_card/>
-    <Laptop_card/>
-    <Laptop_card/>
-    </div>
-    <div className=''>
-    <p className='text-[40px] font-bold text-center'>Frequently Asked Questions</p>
-    <p className='text-[14px] line-clamp-2 text-center pr-96 pl-96'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus accusantium error eius amet quaerat nostrum ducimus voluptas, eum deserunt? Ab facilis animi doloribus nulla molestias atque, quos odio sequi repellendus.</p>
-    <div className='flex justify-center mt-4'>
-    <form className='w-full max-w-md'>
-      <div className='relative flex items-center text-gray-400 focus-within:text-gray-600 justify-between'>
-      <Input type='text' name='search' placeholder='Search' className='px-3 py-4 font-medium placeholder-gray-500ntext-black border-none ring-2 ring-gray-300 focus:ring-gray-500 focus:ring-2'/>
-      <IoMdSearch className='w-7 h-7 absolute ml-96  ' color="D61837"/>
-      </div>
-      </form>
-    </div>
-    <div className="flex flex-col gap-4 pt-10">
-      <div className="flex font-semibold text-[16px] justify-center">
-      {['General', 'Trust and Safety', 'Services'].map((label, index) => (
-        <Button
-          key={index}
-          variant="link"
-          className={`${activeButton === index? 'text-red-600' : ''} flex`}
-          onClick={() => handleButtonClick(index)}
-        >
-          {label}
-        </Button>
-      ))}
-      </div>
-      <div className="mt-4 ml-20 mr-20 font-medium text-[14px] mt-4 mb-4">
-  {FAQ_buttoncontents}
-</div>
-
-    </div>
-    
-</div>
+        <div className="flex font-semibold text-[16px] justify-center">
+          {["Description", "Product Guide", "Reviews"].map((label, index) => (
+            <Button
+              key={index}
+              variant="link"
+              className={`${activeButton === index ? "text-red-600" : ""} flex`}
+              onClick={() => handleButtonClick(index)}
+            >
+              {label}
+            </Button>
+          ))}
         </div>
+        <div className="mt-4 ml-20 mr-20 font-medium text-[14px]">
+          {buttonContents[activeButton]}
+        </div>
+      </div>
+      <p className="text-[40px] font-bold text-center">Related Products</p>
+      <div className="flex gap-4 items-center justify-center">
+        <Laptop_card />
+        <Laptop_card />
+        <Laptop_card />
+      </div>
+      <div className="">
+        <p className="text-[40px] font-bold text-center">
+          Frequently Asked Questions
+        </p>
+        <p className="text-[14px] line-clamp-2 text-center pr-96 pl-96">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Necessitatibus accusantium error eius amet quaerat nostrum ducimus
+          voluptas, eum deserunt? Ab facilis animi doloribus nulla molestias
+          atque, quos odio sequi repellendus.
+        </p>
+        <div className="flex justify-center mt-4">
+          <form className="w-full max-w-md">
+            <div className="relative flex items-center text-gray-400 focus-within:text-gray-600 justify-between">
+              <Input
+                type="text"
+                name="search"
+                placeholder="Search"
+                className="px-3 py-4 font-medium placeholder-gray-500ntext-black border-none ring-2 ring-gray-300 focus:ring-gray-500 focus:ring-2"
+              />
+              <IoMdSearch className="w-7 h-7 absolute ml-96  " color="D61837" />
+            </div>
+          </form>
+        </div>
+        <div className="flex flex-col gap-4 pt-10">
+          <div className="flex font-semibold text-[16px] justify-center">
+            {["General", "Trust and Safety", "Services"].map((label, index) => (
+              <Button
+                key={index}
+                variant="link"
+                className={`${
+                  activeButton === index ? "text-red-600" : ""
+                } flex`}
+                onClick={() => handleButtonClick(index)}
+              >
+                {label}
+              </Button>
+            ))}
+          </div>
+          <div className="mt-4 ml-20 mr-20 font-medium text-[14px] mt-4 mb-4">
+            {FAQ_buttoncontents}
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Footer from '@/components/Footer';
-import Header from '@/components/Header';
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,12 +17,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      
-      <body className={`${inter.className}`} >
-      <Header />
+      <body className={`${inter.className}`}>
+        <Header />
         {children}
-        <Footer/>
-        </body>
+        <Footer />
+      </body>
     </html>
   );
 }
