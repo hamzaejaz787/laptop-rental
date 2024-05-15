cd1a1cf15bea2c9addf6a9cb67a6f7a472e9d2c4
hamzaejaz787 <hejaz389@gmail.com>
2024-05-15 10:01:33 +0500

    header and contact page changes

--- components/Header.tsx
+++ components/Header.tsx
@@ -8,30 +8,21 @@ import { MdAddShoppingCart, MdOutlineLocationOn } from "react-icons/md";
 function Header() {
   return (
     <div className="fixed left-0 right-0 top-0 z-50">
-      {/* <div className="relative"> */}
-      <div className="w-full lg:w-full h-[50px] bg-[#D61837]"></div>
-      <div className="absolute">
+      <div className="w-full lg:w-full h-12 bg-[#D61837]"></div>
+      {/* <div className=""> */}
+
+      {/* </div> */}
+      <div className="flex relative justify-between items-center px-10 py-2">
+        {" "}
+        <Image src="/Vector.png" alt="" fill={true} className="-z-10" />
         <Image
-          src="/Vector.png"
-          alt=""
-          height={250}
-          width={5000}
+          src="/laptop_rental_logo.png"
+          alt="laptop Logo"
+          width={120}
+          height={50}
           className=""
         />
-      </div>
-      <div className="flex relative justify-between items-center pl-10 pr-10 pt-3">
-        <div>
-          <Image
-            src="/laptop_rental_logo.png"
-            alt="laptop Logo"
-            height={40.53}
-            width={100}
-            className="flex items-center"
-          />
-        </div>
-        <div>
-          <Navbar />
-        </div>
+        <Navbar />
         <div className="flex items-center gap-2 pr-2">
           <Button>Get a quote</Button>
           <MdOutlineLocationOn size={25} />
