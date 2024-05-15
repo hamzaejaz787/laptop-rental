d0ae9c52bf26384ec24b355c911fc3eca7f672a8
hamzaejaz787 <hejaz389@gmail.com>
2024-05-15 15:43:36 +0500

    header and contact page changes

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,24 +4,18 @@ import { Navbar } from "@/components/navbar";
 import { Button } from "@/components/ui/button";
 import { IoSearchSharp } from "react-icons/io5";
 import { MdAddShoppingCart, MdOutlineLocationOn } from "react-icons/md";
-import { HiBars3BottomRight } from "react-icons/hi2";
-
 
 function Header() {
   return (
     <div className="fixed left-0 right-0 top-0 z-50">
-      <div className="w-full lg:w-full h-12 bg-[#D61837]"></div>
-      {/* <div className=""> */}
-
-      {/* </div> */}
-      <div className="flex relative justify-between items-center px-10 py-2">
-        {" "}
-        <Image src="/Vector.png" alt="" fill={true} className="-z-10" />
+      {/* <div className="relative"> */}
+      <div className="w-full lg:w-full h-[50px] bg-[#D61837]"></div>
+      <div className="absolute">
         <Image
-          src="/laptop_rental_logo.png"
-          alt="laptop Logo"
-          width={120}
-          height={50}
+          src="/Vector.png"
+          alt=""
+          height={250}
+          width={5000}
           className=""
         />
       </div>
@@ -38,13 +32,13 @@ function Header() {
         <div className="flex pl-10 pr-10">
           <Navbar />
         </div>
-        <Navbar />
         <div className="flex items-center gap-2 pr-2">
-          <Button className='font-Poppins text-[12px] border border-none bg-[#D61837]'>Get a quote</Button>
+          <Button className="font-Poppins text-[12px] border border-none bg-[#D61837]">
+            Get a quote
+          </Button>
           <MdOutlineLocationOn size={20} />
           <MdAddShoppingCart size={20} />
           <IoSearchSharp size={20} />
-          <HiBars3BottomRight  size={25} className="md:hidden"/>
         </div>
       </div>
       {/* </div> */}
